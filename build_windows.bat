338a9db3ce6cf4be6f92399f0a7430d4c1effe6a
JeremyRand <biolizard89@gmail.com>
2014-09-11 17:52:07 -0500

    Auto DNS config files are now included in the Windows distribution.

--- build_windows.bat
+++ build_windows.bat
@@ -7,3 +7,7 @@ mkdir dist\nmcontrol\service\
 xcopy lib dist\nmcontrol\lib /s /e /h
 xcopy plugin dist\nmcontrol\plugin /s /e /h
 xcopy service dist\nmcontrol\service /s /e /h
+
+copy local_bit_dns_start.reg dist\nmcontrol\
+copy local_bit_dns_stop.reg dist\nmcontrol\
+copy nmcontrol_auto_dns.bat dist\nmcontrol\
